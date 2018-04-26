eb402bacf892130c5d6ad2afb5146844b513c7c6
Edward Carlson <plupiman@cock.li>
2018-04-26 11:43:58 -0700

    commiting during dig inst

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function search () {
     var url = "player/player.html";
     document.location.href = url;
+    let actualId = document.getElementById("searchTerm").value;
+    localStorage.setItem("dotaID", actualId);
 }
---
+++ player/player.js
@@ -0,0 +1,8 @@
+let API_URL = "https://api.opendota.com/api/";
+
+function setup(){
+    let dotaID = localStorage.getItem("dotaID");
+    console.log(dotaID);
+}
+
+setup();
