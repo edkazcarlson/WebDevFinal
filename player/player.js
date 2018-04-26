424979d5de78452676bf9c4b9aef996045246792
Edward Carlson <plupiman@cock.li>
2018-04-26 14:35:29 -0700

    commiting my mmr reader

--- player/player.js
+++ player/player.js
@@ -3,6 +3,39 @@ let API_URL = "https://api.opendota.com/api/";
 function setup(){
     let dotaID = localStorage.getItem("dotaID");
     console.log(dotaID);
+
+	let url = API_URL + "players/" + dotaID + "/";
+	let oReq = new XMLHttpRequest();
+	oReq.onreadystatechange = function handleReady(){
+		if (this.readyState == 4 && this.status == 200) {
+			let rank = JSON.parse(this.responseText);
+			console.log(rank.solo_competitive_rank);
+		}
+	}
+	oReq.open("GET", url, true);
+	oReq.send();
+
+
+
+	//requestHandler = new RequestHandler();
+    //requestHandler.makeRequest("GET", url, )
 }
 
+/*
+
+
+
+class RequestHandler {
+	makeRequest(method, url, handler) {
+		let request = new XMLHttpRequest();
+		request.onreadystatechange = function handleReady() {
+			if (this.readyState == 4 && this.status == 200) {
+				handler(this.responseText);
+			}
+		}
+		request.open(method, url, true);
+		request.send();
+	}
+}
+*/
 setup();
