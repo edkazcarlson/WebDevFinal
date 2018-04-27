bf41690acc0f979d6ce004526f36caa2cdac7776
kaimihata <kaimihata@gmail.com>
2018-04-27 17:24:56 -0400

    added request handler and win/loss counts

--- player/player.js
+++ player/player.js
@@ -4,30 +4,45 @@ function setup(){
     let dotaID = localStorage.getItem("dotaID");
     console.log(dotaID);
 
+	//testing requesthandler
+	const requestHandler = new RequestHandler();
+
 	let url = API_URL + "players/" + dotaID + "/";
 	let oReq = new XMLHttpRequest();
 	oReq.onreadystatechange = function handleReady(){
 		if (this.readyState == 4 && this.status == 200) {
 			let rank = JSON.parse(this.responseText);
 			console.log(rank.solo_competitive_rank);
-			let profile = rank.profile;
-			document.getElementById("personaName").innerHTML = profile.personaname;
 			document.getElementById("container stats__display").innerHTML = rank.solo_competitive_rank;
-			document.getElementById("profilePic").src = profile.avatarmedium;
 		}
 	}
+
+	//this is how u make a request so u dont have to do the onreadystate change thing every time
+	//gets information from 
+	requestHandler.makeRequest("GET", url, function personaName(data) {
+		const player = JSON.parse(data);
+		let profile = player.profile;
+		console.log(profile);
+		document.getElementById("personaName").innerHTML = profile.personaname;
+		document.getElementById("profilePic").src = profile.avatarmedium;
+	});
+
 	oReq.open("GET", url, true);
 	oReq.send();
 
-
+	winLoss(url, requestHandler);
 
 	//requestHandler = new RequestHandler();
     //requestHandler.makeRequest("GET", url, )
 }
 
-/*
-
-
+function winLoss(url, requestHandler) {
+	requestHandler.makeRequest("GET", url + "wl", function getWinLoss(data){
+		let wl = JSON.parse(data);
+		document.getElementById("win").innerHTML = "Wins: " +  wl.win;
+		document.getElementById("loss").innerHTML = "Losses: " + wl.lose;
+	});
+}
 
 class RequestHandler {
 	makeRequest(method, url, handler) {
@@ -41,5 +56,5 @@ class RequestHandler {
 		request.send();
 	}
 }
-*/
+
 setup();
